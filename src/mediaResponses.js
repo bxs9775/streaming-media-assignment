2b9cdf1d5c0a982174e4aeb7df24a7aaa869ca4f
BRIAN SANDON (RIT Student) <bxs9775@ad.rit.edu>
2018-01-24 14:13:34 -0500

    Added Node.js project. * created server.js, htmlResponses.js, and mediaResponses.js files

---
+++ src/mediaResponses.js
@@ -0,0 +1,10 @@
+//require/import file system module
+const fs = require('fs');
+//require/import path module
+const path = require('path');
+
+const getParty = (request, response) => {
+  
+};
+
+module.exports.getParty = getParty;
